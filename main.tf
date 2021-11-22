e788ca60443b1f6663941dad9c1ac2ecf7a47755
Mikhail Kalmykov <mikhail_kalmykov@epam.com>
2021-11-22 11:20:37 +0300

    EKS cluster creation implemented

--- main.tf
+++ main.tf
@@ -41,26 +41,58 @@ module "vpc" {
 
 }
 
-module "sg_web_ssh" {
-  source = "terraform-aws-modules/security-group/aws"
-  name   = "sg_web_ssh"
-  vpc_id = module.vpc.vpc_id
-
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  ingress_rules = ["http-80-tcp", "ssh-tcp"]
-  egress_rules  = ["all-all"]
+module "eks" {
+  source           = "terraform-aws-modules/eks/aws"
+  cluster_name     = "my_eks"
+  cluster_version  = "1.21"
+  vpc_id           = module.vpc.vpc_id
+  subnets          = module.vpc.public_subnets
+  write_kubeconfig = false
+  worker_groups = [
+    {
+      instance_type        = "t2.small"
+      key_name             = "mskawslearn1-pair1"
+      asg_desired_capacity = 1
+      asg_max_size         = 3
+    }
+  ]
 }
 
-module "app_server" {
-  source = "terraform-aws-modules/ec2-instance/aws"
-
-  name                        = "tf-course-ec2"
-  ami                         = "ami-0a49b025fffbbdac6"
-  instance_type               = "t2.micro"
-  key_name                    = "mskawslearn1-pair1"
-  associate_public_ip_address = "true"
-  subnet_id                   = module.vpc.public_subnets[0]
-  vpc_security_group_ids      = [module.sg_web_ssh.security_group_id]
-
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
 }
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+
+#module "sg_web_ssh" {
+#  source = "terraform-aws-modules/security-group/aws"
+#  name   = "sg_web_ssh"
+#  vpc_id = module.vpc.vpc_id
+#
+#  ingress_cidr_blocks = ["0.0.0.0/0"]
+#
+#  ingress_rules = ["http-80-tcp", "ssh-tcp"]
+#  egress_rules  = ["all-all"]
+#}
+
+#module "app_server" {
+#  source = "terraform-aws-modules/ec2-instance/aws"
+#
+#  name                        = "tf-course-ec2"
+#  ami                         = "ami-0a49b025fffbbdac6"
+#  instance_type               = "t2.micro"
+#  key_name                    = "mskawslearn1-pair1"
+#  associate_public_ip_address = "true"
+#  subnet_id                   = module.vpc.public_subnets[0]
+#  vpc_security_group_ids      = [module.sg_web_ssh.security_group_id]
+#
+#}
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,10 @@
-output "instance_public_ip" {
-  description = "Public IP address of the AppServer"
-  value       = module.app_server.public_ip
+output "cluster_id" {
+  description = "EKS cluster ID."
+  value       = module.eks.cluster_id
 }
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane."
+  value       = module.eks.cluster_endpoint
+}
+
