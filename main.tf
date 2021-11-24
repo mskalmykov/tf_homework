123dbebf58e129a83dc6cdd69f92d70fda625993
Mikhail Kalmykov <mikhail_kalmykov@epam.com>
2021-11-24 12:36:55 +0000

    Errors related to RDS access fixed.

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ module "eks" {
     {
       instance_type        = "t2.small"
       key_name             = "mskawslearn1-pair1"
-      asg_desired_capacity = 2
+      asg_desired_capacity = 1
       asg_max_size         = 3
     }
   ]
@@ -72,12 +72,8 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
-resource "aws_ecr_repository" "my-ecr" {
-  name = "my-ecr"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+resource "aws_ecr_repository" "nhltop" {
+  name = "nhltop"
 }
 
 module "db" {
@@ -93,12 +89,12 @@ module "db" {
 
   name     = "nhltop"
   username = "nhltop"
-  password = "$(var.DB_PASSWORD)"
+  password = var.DB_PASSWORD
   port     = "3306"
 
   skip_final_snapshot = true
 
-  #vpc_security_group_ids = ["sg-12345678"]
+  vpc_security_group_ids = [aws_security_group.sg_rds.id]
 
   # DB subnet group
   subnet_ids           = module.vpc.public_subnets
@@ -107,26 +103,25 @@ module "db" {
 
 }
 
-#module "sg_web_ssh" {
-#  source = "terraform-aws-modules/security-group/aws"
-#  name   = "sg_web_ssh"
-#  vpc_id = module.vpc.vpc_id
-#
-#  ingress_cidr_blocks = ["0.0.0.0/0"]
-#
-#  ingress_rules = ["http-80-tcp", "ssh-tcp"]
-#  egress_rules  = ["all-all"]
-#}
+resource "aws_security_group" "sg_rds" {
+  name        = "RDS security group"
+  description = "Allow tcp/3306 from EKS"
+  vpc_id      = module.vpc.vpc_id
 
-#module "app_server" {
-#  source = "terraform-aws-modules/ec2-instance/aws"
-#
-#  name                        = "tf-course-ec2"
-#  ami                         = "ami-0a49b025fffbbdac6"
-#  instance_type               = "t2.micro"
-#  key_name                    = "mskawslearn1-pair1"
-#  associate_public_ip_address = "true"
-#  subnet_id                   = module.vpc.public_subnets[0]
-#  vpc_security_group_ids      = [module.sg_web_ssh.security_group_id]
-#
-#}
+  ingress {
+    description      = "MySQL from VPC"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = module.vpc.public_subnets_cidr_blocks
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "DB_PASSWORD" {
+  description = "Password for MariaDB user"
+  type        = string
+}
+
